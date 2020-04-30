084bbbae21e66a6c0ddf8c07d85c62af98f4a5a5
IvanaPeneva <55322951+IvanaPeneva@users.noreply.github.com>
2020-04-30 16:47:30 +0300

    Add files via upload

--- simpleCalculatorMethod.java
+++ simpleCalculatorMethod.java
@@ -5,7 +5,7 @@ public class simpleCalculatorMethod {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         ArrayDeque<Integer> stack = new ArrayDeque<>();
-        String[] input = scanner.nextLine().split(" ");
+        String[] input = scanner.nextLine().split("\\s+");
         for (int i=0; i<input.length;i++){
             if (Character.isDigit(input[i].charAt(0))){
                 stack.push(Integer.parseInt(input[i]));
